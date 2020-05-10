0aadf1d4de377046134daba8814ef48d38dac32a
Petter Andersson <petter.andersson@tendium.com>
2020-05-11 01:08:12 +0200

    Updated initializtion method, untested

--- src/index.js
+++ src/index.js
@@ -4,44 +4,57 @@
 const util = require('util')
 const mysql = require('mysql')
 
+const initialize = (DB_HOST, DB_USER, DB_PW, DB_NAME) => {
+  const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PW,
+    database: DB_NAME
+  })
 
-const init = () => {
+  // Ping database to check for common exception errors.
+  pool.getConnection((err, connection) => {
+    if (err) {
+      if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+        console.error('Database connection was closed.')
+      }
+      if (err.code === 'ER_CON_COUNT_ERROR') {
+        console.error('Database has too many connections.')
+      }
+      if (err.code === 'ECONNREFUSED') {
+        console.error('Database connection was refused.')
+      }
+    }
+
+    if (connection) connection.release()
+
+    return;
+  })
+  // Promisify for Node.js async/await.
+  pool.query = util.promisify(pool.query)
+
+  return pool;
+}
+
+module.exports.initialize = (DB_HOST, DB_USER, DB_PW, DB_NAME) => {
+  return initialize(DB_HOST, DB_USER, DB_PW, DB_NAME);
+}
+
+module.exports.initialize = () => {
   if (!process.env.DB_HOST || !process.env.DB_USER || !process.env.DB_PW || !process.env.DB_NAME) {
     console.error('mysql-promisify-pool: Requires env variables: DB_HOST, DB_USER, DB_PW, DB_NAME');
     // Wait for manual initialization
+    console.error('use init with parameters instead');
+    return null;
+  } else {
+    initialize(
+      process.env.DB_HOST,
+      process.env.DB_USER,
+      process.env.DB_PW,
+      process.env.DB_NAME
+    );
   }
 }
 
-init();
-
-const pool = mysql.createPool({
-  connectionLimit: 10,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PW,
-  database: process.env.DB_NAME
-})
-
-// Ping database to check for common exception errors.
-pool.getConnection((err, connection) => {
-  if (err) {
-    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-      console.error('Database connection was closed.')
-    }
-    if (err.code === 'ER_CON_COUNT_ERROR') {
-      console.error('Database has too many connections.')
-    }
-    if (err.code === 'ECONNREFUSED') {
-      console.error('Database connection was refused.')
-    }
-  }
-
-  if (connection) connection.release()
-
-  return
-})
-
-// Promisify for Node.js async/await.
-pool.query = util.promisify(pool.query)
-
-module.exports = pool;
+//module.exports = pool;
